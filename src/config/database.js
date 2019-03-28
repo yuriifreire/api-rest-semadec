3d3da56e80c4422e9dd972491d0f5c9352251ce8
Yuri Freire <yuriifreire@hotmail.com>
2019-03-28 16:07:36 -0300

    Remove root pass

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,3 @@
-
-//npm i sequelize mysql2
-
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize("semadec","root","",{
     host  : "localhost",
@@ -8,13 +5,5 @@ const sequelize = new Sequelize("semadec","root","",{
     port : 3306,
     operatorsAliases: false
 });
-
-/*
-sequelize.authenticate().then(()=>{
-    console.log("Conectado");
-}).catch((erro)=>{
-    console.log("Não conectado");
-});
-*/
 
 module.exports = sequelize;
