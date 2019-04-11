5b9e884e390859289dc62932c877e40f4992478c
Yuri Freire <yuriifreire@hotmail.com>
2019-04-11 16:30:37 -0300

    Add criptografia for users

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,19 +1,45 @@
-const Modalidade = require("../models/usuarios");
+const Livro = require("../models/usuarios");
+const Usuario = require("../models/usuarios");
 const Status = require("http-status");
 const authService = require("../services/auth-service");
+const bcrypt = require('bcrypt');
 
-exports.autenticar = (req,res,next)=>{
+
+exports.criarUsuario = (req, res, next) => {
+    bcrypt.hash(req.body.senha, 10, function (err, hash) {
+        Usuario.create({
+            email: req.body.email,
+            senha: hash
+        }).then(() => {
+            res.status(Status.CREATED).send();
+        }).catch((error) => {
+            next(error);
+        });
+    });
+};
+
+exports.listarUsuario = (req, res, next) => {
+    Usuario.findAll().then((usuarios) => {
+        res.status(Status.OK).send(usuarios);
+    }).catch((error) => {
+        next(error);
+    });
+
+};
+
+exports.autenticar = (req, res, next) => {
+
     const dados = req.body;
-    Modalidade.findAll({
-        where : {
+    Livro.findAll({
+        where: {
             email: dados.email,
             senha: dados.senha
         }
-    }).then((usuario)=>{
-        let token = authService.geraToken({id: usuario[0].id, email: usuario[0].email});
+    }).then((usuario) => {
+        let token = authService.geraToken({ id: usuario[0].id, email: usuario[0].email });
         console.log(token);
         res.status(Status.OK).send(token);
-    }).catch((erro)=>{
-        res.status(Status.UNAUTHORIZED).send({mensagem : "Usuário e senha inválidos"});        
-    });
+    }).catch((erro) => {
+        res.status(Status.UNAUTHORIZED).send({ mensagem: "Usuário e senha inválidos" });
+    })
 };
